9b6eb2d7e4e04fa88b9ee0d3f5054cc7721c58ad
Andreea <41521522+AndreeaCcss@users.noreply.github.com>
2019-08-12 18:08:04 +0200

    Delete b.js

--- b.js
+++
@@ -1,7 +0,0 @@
-module.exports.myName = 'Andrew'
-module.exports.dateOfBirth = '01-09-1980'
-module.exports.sayHello = function(name) {
-    console.log(`Hello ${name}! My name is ${module.exports.myName}`)
-}
-
-// console.log(module.exports)
